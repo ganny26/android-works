0f40f2599e006a8e22ef087cd2c654feb480b376
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-02 08:47:04 +0530

    added fragments to news activity

---
+++ app/src/main/java/com/app/movie/tmdb/NewsArticleActivity.java
@@ -0,0 +1,14 @@
+package com.app.movie.tmdb;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsArticleActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_article);
+    }
+}
