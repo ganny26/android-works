61492d47546c313b88cff121c03f923600e7913d
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-02 08:47:35 +0530

    display message activity added

--- app/src/main/java/com/app/movie/tmdb/DisplayMessageActivity.java
+++ app/src/main/java/com/app/movie/tmdb/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.app.movie.tmdb;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -16,6 +18,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         TextView textView = (TextView)findViewById(R.id.textView);
+        Button articleButton = (Button) findViewById(R.id.button_show_article);
         textView.setText(message);
+        articleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent articleIntent = new Intent(DisplayMessageActivity.this,NewsArticleActivity.class);
+                startActivity(articleIntent);
+            }
+        });
     }
+
 }
