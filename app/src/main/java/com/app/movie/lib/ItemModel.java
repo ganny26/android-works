802eefdec8e9f350d6e193b44e91f1f257784694
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-02 08:46:36 +0530

    smooth scroll

---
+++ app/src/main/java/com/app/movie/lib/ItemModel.java
@@ -0,0 +1,28 @@
+package com.app.movie.lib;
+
+/**
+ * Created by serajam on 6/2/2017.
+ */
+
+public class ItemModel {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    private String imagePath;
+
+}
