a1fb04a5341a6c4c0505b61275d27fe21792c707
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-03 16:08:34 +0530

    pinterest implementation

---
+++ app/src/main/java/com/app/movie/tmdb/PinterestActivity.java
@@ -0,0 +1,13 @@
+package com.app.movie.tmdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PinterestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pinterest);
+    }
+}
