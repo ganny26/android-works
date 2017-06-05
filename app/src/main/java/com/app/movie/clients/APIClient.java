1fbcba10009df782527de021f79e66afadba4289
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-05 22:48:41 +0530

    movie pojo added

--- app/src/main/java/com/app/movie/clients/APIClient.java
+++ app/src/main/java/com/app/movie/clients/APIClient.java
@@ -1,16 +1,18 @@
 package com.app.movie.clients;
 
-import android.os.AsyncTask;
+
+
 
 /**
  * Created by serajam on 6/4/2017.
  */
 
-public class APIClient extends AsyncTask{
+public class APIClient {
     public static final String DISCOVER_MOVIE = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c79895ea6c454b50e1c7ce72f887f24c";
 
-    @Override
-    protected Object doInBackground(Object[] params) {
-        return null;
-    }
+
+
+
+
+
 }
--- app/src/main/java/com/app/movie/lib/MovieObject.java
+++ app/src/main/java/com/app/movie/lib/MovieObject.java
@@ -5,4 +5,42 @@ package com.app.movie.lib;
  */
 
 public class MovieObject {
+    private String title;
+    private String releaseDate;
+    private String posterPath;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    private String rating;
+
 }
